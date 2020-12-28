66bd7f3880a3714d65f4ccea66e62f62f0c6ec12
Alejandro <alvarezcasoalejandro@gmail.com>
2020-12-28 12:03:37 +0100

    cambios para typescript

---
+++ Back-end/README.md
@@ -0,0 +1,5 @@
+docker-compose build crea los contenedores.
+
+carpeta dist/ tiene los .js
+
+con ts-node ejecutamos los archivos .ts en desarrollo, en el servidor utilizar los .js de dist
---
+++ Back-end/src/index.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+// Inicializacion
+const app = express();
+
+// Configuracion
+app.set('port', 3000);
+
+// Starting server
+app.listen(app.get('port'), () => 
+    console.log(`Servidor en puerto ${app.get('port')}`));
